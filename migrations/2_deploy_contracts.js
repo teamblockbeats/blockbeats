263da3d98bd6bc64246ae12bc4f57b05300f3274
George Mulhearn <gmulhearne@anonyome.com>
2021-04-14 21:16:52 +1000

    compiles and deploys

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,7 @@
 var SimpleStorage = artifacts.require("./SimpleStorage.sol");
+var Blockbeats = artifacts.require("./Blockbeats.sol");
 
 module.exports = function(deployer) {
   deployer.deploy(SimpleStorage);
+  deployer.deploy(Blockbeats);
 };
