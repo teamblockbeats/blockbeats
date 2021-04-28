36cbf6a8e867c965060418c62cf800b721da9377
George Mulhearn <gmulhearne@anonyome.com>
2021-04-28 10:34:34 +1000

    can resolve correctly

--- client/src/components/Verify.js
+++ client/src/components/Verify.js
@@ -6,11 +6,7 @@ import TextField from "@material-ui/core/TextField";
 import FormControl from "@material-ui/core/FormControl";
 import CheckCircleIcon from "@material-ui/icons/CheckCircle";
 import CancelIcon from "@material-ui/icons/Cancel";
-import {
-  Button,
-  Typography,
-  Box
-} from "@material-ui/core";
+import { Button, Typography, Box, CircularProgress } from "@material-ui/core";
 import Web3 from "web3";
 import BlockBeats from "../contracts/Blockbeats.json";
 
@@ -33,12 +29,13 @@ const Verify = () => {
   const [contract, setContract] = useState(null);
 
   const [user, setUser] = useState(null);
-  const [listingID, setListingID] = useState(null);
-
+  const [listingIDField, setlistingIDField] = useState(null);
 
   const [checkedUser, setCheckedUser] = useState("");
-  const [checkedListing, setCheckedListing] = useState("")
+  const [checkedListing, setCheckedListing] = useState("");
   const [verified, setVerified] = useState(null); // type: Boolean?
+
+  const [loading, setLoading] = useState(false);
 
   /******************* BOOTSTRAPPING *****************/
   useEffect(() => {
@@ -79,17 +76,49 @@ const Verify = () => {
   };
   /******************************************************/
 
+  const concatAddress = (totalLength, addr) => {
+    let x = (totalLength - 3) / 2;
+    return (
+      addr.substr(0, x + 1) +
+      "..." +
+      addr.substring(addr.length - x, addr.length)
+    );
+  };
+
+  const addrOwnsListing = async (addr, listingID) => {
+    let tokenIds = await contract.methods
+      .tokensAtAddress(addr)
+      .call({ from: account });
+
+    for (const tokenId of tokenIds) {
+      console.log(tokenId);
+      let listing = await contract.methods
+        .resolveTokenToListing(tokenId)
+        .call({ from: account });
+
+      console.log("listing[id]");
+      console.log(listing["id"]);
+
+      if (listing["id"] == listingID) {
+        return true;
+      }
+    }
+
+    return false;
+  };
+
   const handleVerify = async () => {
     console.log("verify");
-    console.log(Number(listingID));
+    console.log(Number(listingIDField));
     console.log(user);
-    setCheckedUser(user)
-    setCheckedListing(Number(listingID))
-    if (verified == null) {
-      setVerified(true);
-    } else {
-      setVerified(!verified);
-    }
+    setLoading(true)
+    setVerified(
+      (await addrOwnsListing(user, Number(listingIDField))).valueOf()
+    );
+
+    setCheckedUser(concatAddress(10, user));
+    setCheckedListing(Number(listingIDField));
+    setLoading(false)
   };
 
   const drawVerifyInputFields = () => {
@@ -103,27 +132,54 @@ const Verify = () => {
         <TextField
           placeholder="Enter Unique Listing ID..."
           variant="outlined"
-          onChange={(e) => setListingID(e.target.value)}
+          onChange={(e) => setlistingIDField(e.target.value)}
         />
       </FormControl>
     );
   };
 
   const drawVerified = () => {
+    if (loading) {
+      return (
+        <Box display="flex" justifyContent="center" marginTop={2}>
+          <CircularProgress />
+        </Box>
+      );
+    }
     if (verified == null) {
       return;
     } else if (verified) {
       return (
-        <Box display="flex" justifyContent="center" flexDirection="row" marginTop={2}>
-          <CheckCircleIcon color="primary" style={{marginRight: 5}} fontSize="large"/>
-          <Typography style={{margin: 5}}><b>Verified!</b>  User "{checkedUser}" has purchased a license for listing ID: {checkedListing}</Typography>
+        <Box
+          display="flex"
+          justifyContent="center"
+          flexDirection="row"
+          marginTop={2}
+        >
+          <CheckCircleIcon
+            color="primary"
+            style={{ marginRight: 5 }}
+            fontSize="large"
+          />
+          <Typography style={{ margin: 5 }}>
+            <b>Verified!</b> User "{checkedUser}" has purchased a license for
+            listing ID: {checkedListing}
+          </Typography>
         </Box>
       );
     } else {
       return (
-        <Box display="flex" justifyContent="center" flexDirection="row" marginTop={2}>
-          <CancelIcon color="secondary" fontSize="large"/>
-          <Typography style={{margin: 5}}><b>Unverified.</b>  User "{checkedUser}" has <b>not</b> purchased a license for listing ID: {checkedListing}</Typography>
+        <Box
+          display="flex"
+          justifyContent="center"
+          flexDirection="row"
+          marginTop={2}
+        >
+          <CancelIcon color="secondary" fontSize="large" />
+          <Typography style={{ margin: 5 }}>
+            <b>Unverified.</b> User "{checkedUser}" has <b>not</b> purchased a
+            license for listing ID: {checkedListing}
+          </Typography>
         </Box>
       );
     }
